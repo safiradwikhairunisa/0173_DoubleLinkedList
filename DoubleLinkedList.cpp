c214653a765287be711234b3b8023575c885a21e
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-15 10:25:38 +0700

    menambah if

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -82,5 +82,12 @@ class DoubleLinkedList
         currrent->next = newNode; //step 9d: current.next = newNode
     }
 
-    
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+    }
 }
