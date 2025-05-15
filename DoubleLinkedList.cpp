81785ab9446cdb843bb347b51cdbd051a0867d81
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-15 09:48:53 +0700

    menambah class doublelinkedlist

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -9,3 +9,21 @@ class Node
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+    private:
+    Node *START;
+
+    public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        string nm;
+    }
+}
