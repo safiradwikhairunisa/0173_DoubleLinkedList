6b006d41c773256623fb4e822d4146857e96e92e
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-15 11:24:40 +0700

    selesai

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -239,7 +239,21 @@ int main()
             case '3':
                 list.traverse();
                 break;
+            case '4':
+                list.revtraverse();
+                break;
+            case '5':
+                list.searchData();
+                break;
+            case '6':
+                return 0;
+            default:
+                cout << "Invalid option\n";
         }
-    }
-    
+        cout << "\nPress enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+    }while (choice != '6');
 }
